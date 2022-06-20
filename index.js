2ff2780f28477ff093c39f636392001e9ac1edc3
Cássia Costa <cassia.silva93@hotmail.com>
2022-06-20 16:44:54 -0300

    Função de ler arquivo .md

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs');
+
+function trataErro(erro) {
+  throw new Error(erro.code, 'não há arquivo no caminho!');
+}
+
+function pegaArquivo(caminhoDoArquivo) {
+  const encoding = 'utf-8';
+  fs.promises
+  .readFile(caminhoDoArquivo, encoding)
+  .then((texto) => console.log(texto))
+  .catch((erro) => trataErro(erro))
+}
+
+// function pegaArquivo(caminhoDoArquivo) {
+//   const encoding = 'utf-8';
+//   fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//     if(erro) {
+//       trataErro(erro);
+//     }
+//     console.log(texto)
+//   })
+// }
+
+pegaArquivo('./src/text1.md');
